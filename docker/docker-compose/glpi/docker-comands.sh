8f0bab634b74d23d5a0e91dd61312f46d6048b93
Rafael Tadeu <rtadeu@mmc.local>
2020-05-09 03:38:37 -0300

    update

--- docker/docker-compose/glpi/docker-comands.sh
+++
@@ -1,5 +0,0 @@
-
-# Creating database dumps
-#
-# docker exec some-mysql sh -c 'exec mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PA
-# docker run -it --rm -v /bkpmysql:/bkpmysql mysql:5.7 sh -c 'exec mysqldump -h host -uroot -p glpi > /bkpmysql/glpi.sql'
